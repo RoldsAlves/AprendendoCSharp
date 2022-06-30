f8ce74bfebbe36edbd4b87a35e7d59bae62aa679
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2022-06-30 20:27:46 -0300

    Variaveis de Texto

---
+++ 5-CaracteresETexto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _5_CaracteresETexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 5 - Caracteres e Textos.");
+
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            letra = (char)65;
+            Console.WriteLine(letra);
+
+            letra = (char)(65 + 1);
+            Console.WriteLine(letra);
+
+            string primeiraFrase = "Alura - Cursos de Tecnologia ";
+            Console.WriteLine(primeiraFrase + 2022);
+
+            string vazia = "";
+            Console.WriteLine(vazia);
+
+            letra = ' ';
+            Console.WriteLine(letra);
+
+            string cursos = @"Cursos disponíveis: 
+- GO 
+- C# 
+- Python 
+- Java";
+            Console.WriteLine(cursos);
+
+
+            Console.WriteLine("Tecle Enter para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 6 - Atribuições de Variáveis.");
+
+            int idade = 30;
+            int idadeAna = idade;
+            Console.WriteLine(idadeAna);
+
+            idade = 25;
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Tecle Enter para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
