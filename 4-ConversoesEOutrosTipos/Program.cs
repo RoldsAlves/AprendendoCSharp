6a392027177079bdc66be60f3386a23fb0491b5a
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2022-06-30 17:30:34 -0300

    C# Explorando as linguagens - Variáveis numéricas

--- 4-ConversoesEOutrosTipos/Program.cs
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -6,7 +6,25 @@ namespace _4_ConversoesEOutrosTipos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Projeto 4 - Conversões e Outros Tipos.");
+            double salario = 3200.10;
+            Console.WriteLine(salario);
+
+            int salarioInteiro = (int)salario;
+            Console.WriteLine(salarioInteiro);
+
+            //O long é um tipo de variável que suporta até 64 bits
+            long x = 2000000000000000;
+            Console.WriteLine(x);
+
+            short y = 15000;
+            Console.WriteLine(y);
+
+            float altura = 1.62f;
+            Console.WriteLine(altura);
+
+            Console.WriteLine("Tecle Enter para fechar...");
+            Console.ReadLine();
         }
     }
 }
