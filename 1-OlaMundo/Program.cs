572a44061896a3184718980d8b65a32b0b036969
Rodolfo Alves <rodolfo.s.a@hotmail.com>
2022-06-30 12:12:53 -0300

    Adicionar arquivos de projeto.

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 1 - Olá Mundo!");
+            Console.WriteLine("Tecle Enter para fechar.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3-PontosFlutuantes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_PontosFlutuantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 3 - Variáveis Pontos Flutuantes.");
+            double salario;
+            salario = 3200.10;
+            Console.WriteLine("Salário é " + salario);
+            salario = 3000;
+            Console.WriteLine("Salário é " + salario);
+
+            double idade;
+            idade = 7.0/5;
+            Console.WriteLine("Minha idade é " + idade);
+
+            Console.WriteLine("Tecle Enter para fechar...");
+            Console.ReadLine();
+        }
+    }
+}
